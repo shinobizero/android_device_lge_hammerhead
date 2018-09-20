7fb7a6b5cd7650f6a163025956cf44962a8ce801
shinobizero <shinobizero@gmx.com>
2018-09-20 20:03:14 +1000

    Added device identifyer stuff

--- zero.mk
+++ zero.mk
@@ -55,6 +55,13 @@ PRODUCT_COPY_FILES += \
 # Inherit device configuration
 $(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
 
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := hammerhead
+PRODUCT_NAME := zero_hammerhead
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 5
+PRODUCT_MANUFACTURER := LGE
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=hammerhead \
     BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/M4B30Z/3437181:user/release-keys \

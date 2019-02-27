28cbb4ed672d88cc6b99bdcb8c78cc99e9f556a0
shinobizero <shinobizero@gmx.com>
2019-02-27 15:35:46 +1000

    Added zero.mk

--- zero.mk
+++ zero.mk
@@ -1,17 +1,18 @@
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1080
-TARGET_BOOTANIMATION_HALF_RES := true
+ZERO_PRODUCT_DEVICE ?= hammerhead
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common ZeroROM stuff.
+$(call inherit-product, vendor/zero/config/phone.mk)
+
+# ElementalX-specific init file
+PRODUCT_COPY_FILES += \
+    kernel/lge/hammerhead-ELEMENTALX/init.elementalx.rc:root/init.elementalx.rc
 
 # Inherit device configuration
 $(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hammerhead
-PRODUCT_NAME := lineage_hammerhead
+PRODUCT_NAME := zero_hammerhead
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 5
 PRODUCT_MANUFACTURER := LGE
